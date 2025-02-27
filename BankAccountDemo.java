8f01167e881018d180217d2f8f9e3ed92486b30a
joric10 <balconjoric_bscs@plmun.edu.ph>
2025-02-27 18:11:16 +0800

    Submitting OBJPROG Week 05 - Laboratory # 23

---
+++ BankAccountDemo.java
@@ -0,0 +1,55 @@
+class BankAccount {
+    // Attributes
+    public String accountHolder;
+    private double balance;
+    protected String accountType;
+
+    // Constructor
+    public BankAccount(String accountHolder, double balance, String accountType) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
+
+    // Method to deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Method to withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient funds.");
+        }
+    }
+
+    // Method to get balance
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class BankAccountDemo {
+    public static void main(String[] args) {
+        // Creating a BankAccount object
+        BankAccount myAccount = new BankAccount("Alice", 1000.0, "Checking");
+
+        // Performing transactions
+        myAccount.deposit(250.0);
+        myAccount.withdraw(100.0);
+
+        // Displaying account details
+        System.out.println("\nAccount Details:");
+        System.out.println("Account Holder: " + myAccount.accountHolder);
+        System.out.println("Account Type: " + myAccount.accountType);
+        System.out.println("Current Balance: $" + myAccount.getBalance());
+    }
+}
